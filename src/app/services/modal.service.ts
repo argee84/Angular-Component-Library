caf91dd2118e83ad3ca820648495ce3f332b223a
Artur Beyrle <artur@lexip-design.de>
2018-12-23 13:27:49 +0100

    refactor sass

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -1,33 +1,30 @@
-import {
-  Injectable,
-  Renderer2,
-  RendererFactory2 }  from '@angular/core';
+import { Injectable, Renderer2, RendererFactory2 } from '@angular/core';
 import { DomService } from './dom.service';
 
 
 @Injectable()
 export class ModalService {
 
-  constructor( private _domService: DomService, private rendererFactory: RendererFactory2 ) {   
+  constructor( private _domService: DomService, private rendererFactory: RendererFactory2 ) {
     this.renderer = rendererFactory.createRenderer(null, null);
-   }
+  }
 
-  private modalWrapperId = 'modal-wrapper';
-  private modalElementId = 'modal-container';
-  private overlayElementId = 'overlay';
+  private modalWrapperId = 'modal';
+  private modalElementId = 'modal-content';
+  private overlayElementId = 'modal-overlay';
   private renderer: Renderer2;
 
   init(component: any, inputs: object, outputs: object) {
     let componentConfig = {
       inputs:inputs,
       outputs:outputs
-    }
+    };
     this._domService.appendComponentTo(this.modalElementId, component, componentConfig);
     document.getElementById(this.modalWrapperId).className = 'show';
     document.getElementById(this.modalElementId).className = 'show';
     document.getElementById(this.overlayElementId).className = 'show';
 
-    this.renderer.addClass(document.body, 'modal-openend'); 
+    this.renderer.addClass(document.body, 'modal-openend');
   }
 
   destroy() {
@@ -36,6 +33,6 @@ export class ModalService {
     document.getElementById(this.modalElementId).className = 'hidden';
     document.getElementById(this.overlayElementId).className = 'hidden';
 
-    this.renderer.removeClass(document.body, 'modal-openend')
+    this.renderer.removeClass(document.body, 'modal-openend');
   }
 }
